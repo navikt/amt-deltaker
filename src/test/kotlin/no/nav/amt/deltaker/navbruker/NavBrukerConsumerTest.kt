18a9ca47450e71018951f78c4fc8294c03f53d3a
Tia Firing <tia.firing@nav.no>
2024-02-16 08:51:59 +0100

    henter enhet hvis den mangler

--- src/test/kotlin/no/nav/amt/deltaker/navbruker/NavBrukerConsumerTest.kt
+++ src/test/kotlin/no/nav/amt/deltaker/navbruker/NavBrukerConsumerTest.kt
@@ -3,22 +3,33 @@ package no.nav.amt.deltaker.navbruker
 import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.runBlocking
 import no.nav.amt.deltaker.application.plugins.objectMapper
+import no.nav.amt.deltaker.navansatt.navenhet.NavEnhetRepository
+import no.nav.amt.deltaker.navansatt.navenhet.NavEnhetService
 import no.nav.amt.deltaker.utils.SingletonPostgresContainer
 import no.nav.amt.deltaker.utils.data.TestData
 import no.nav.amt.deltaker.utils.data.TestRepository
+import no.nav.amt.deltaker.utils.mockAmtPersonServiceClientNavEnhet
+import org.junit.Before
 import org.junit.BeforeClass
 import org.junit.Test
 
 class NavBrukerConsumerTest {
     companion object {
-        lateinit var repository: NavBrukerRepository
+        lateinit var navBrukerRepository: NavBrukerRepository
+        lateinit var navEnhetRepository: NavEnhetRepository
 
         @JvmStatic
         @BeforeClass
         fun setup() {
             SingletonPostgresContainer.start()
-            repository = NavBrukerRepository()
+            navBrukerRepository = NavBrukerRepository()
+            navEnhetRepository = NavEnhetRepository()
         }
+    }
+
+    @Before
+    fun cleanDatabase() {
+        TestRepository.cleanDatabase()
     }
 
     @Test
@@ -28,13 +39,16 @@ class NavBrukerConsumerTest {
         val navAnsatt = TestData.lagNavAnsatt()
         TestRepository.insert(navAnsatt)
         val navBruker = TestData.lagNavBruker(navEnhetId = navEnhet.id, navVeilederId = navAnsatt.id)
-        val navBrukerConsumer = NavBrukerConsumer(repository)
+        val navBrukerConsumer = NavBrukerConsumer(
+            navBrukerRepository,
+            NavEnhetService(navEnhetRepository, mockAmtPersonServiceClientNavEnhet(navEnhet)),
+        )
 
         runBlocking {
             navBrukerConsumer.consume(navBruker.personId, objectMapper.writeValueAsString(TestData.lagNavBrukerDto(navBruker)))
         }
 
-        repository.get(navBruker.personId).getOrNull() shouldBe navBruker
+        navBrukerRepository.get(navBruker.personId).getOrNull() shouldBe navBruker
     }
 
     @Test
@@ -44,16 +58,37 @@ class NavBrukerConsumerTest {
         val navAnsatt = TestData.lagNavAnsatt()
         TestRepository.insert(navAnsatt)
         val navBruker = TestData.lagNavBruker(navEnhetId = navEnhet.id, navVeilederId = navAnsatt.id)
-        repository.upsert(navBruker)
+        navBrukerRepository.upsert(navBruker)
 
         val oppdatertNavBruker = navBruker.copy(fornavn = "Oppdatert NavBruker")
 
-        val navBrukerConsumer = NavBrukerConsumer(repository)
+        val navBrukerConsumer = NavBrukerConsumer(
+            navBrukerRepository,
+            NavEnhetService(navEnhetRepository, mockAmtPersonServiceClientNavEnhet(navEnhet)),
+        )
 
         runBlocking {
             navBrukerConsumer.consume(navBruker.personId, objectMapper.writeValueAsString(TestData.lagNavBrukerDto(oppdatertNavBruker)))
         }
 
-        repository.get(navBruker.personId).getOrNull() shouldBe oppdatertNavBruker
+        navBrukerRepository.get(navBruker.personId).getOrNull() shouldBe oppdatertNavBruker
+    }
+
+    @Test
+    fun `consumeNavBruker - ny navBruker, enhet mangler - henter enhet og lagrer`() {
+        val navEnhet = TestData.lagNavEnhet()
+        val navAnsatt = TestData.lagNavAnsatt()
+        TestRepository.insert(navAnsatt)
+        val navBruker = TestData.lagNavBruker(navEnhetId = navEnhet.id, navVeilederId = navAnsatt.id)
+        val navBrukerConsumer = NavBrukerConsumer(
+            navBrukerRepository,
+            NavEnhetService(navEnhetRepository, mockAmtPersonServiceClientNavEnhet(navEnhet)),
+        )
+
+        runBlocking {
+            navBrukerConsumer.consume(navBruker.personId, objectMapper.writeValueAsString(TestData.lagNavBrukerDto(navBruker)))
+        }
+
+        navBrukerRepository.get(navBruker.personId).getOrNull() shouldBe navBruker
     }
 }
