23370e82570bcd640de3de98d619bd0e2b6f86f2
Izis Frøyhov <izis.froyhov@nav.no>
2024-09-16 12:48:50 +0200

    Setter opp unleash klient Co-authored-by: Line Moseng <line.moseng@nav.no> Co-authored-by: Tia Firing <tia.firing@nav.no> Co-authored-by: Marthe Frogner Holm <marthe.frogner.holm@nav.no>

--- src/main/kotlin/no/nav/amt/deltaker/unleash/UnleashToggle.kt
+++ src/main/kotlin/no/nav/amt/deltaker/unleash/UnleashToggle.kt
@@ -3,7 +3,7 @@ package no.nav.amt.deltaker.unleash
 import io.getunleash.Unleash
 
 class UnleashToggle(
-    private val unleashClient: Unleash
+    private val unleashClient: Unleash,
 ) {
     fun erKometMasterForTiltakstype(tiltakstype: String): Boolean {
         return unleashClient.isEnabled("amt.enable-komet-deltakere") && tiltakstype == "ARBFORB"
