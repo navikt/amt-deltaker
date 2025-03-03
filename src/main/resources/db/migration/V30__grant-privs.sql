d7f781d97cf5ba86cdf6070f79db61ad485a0241
Izis Frøyhov <izis.froyhov@nav.no>
2025-03-03 13:38:03 +0100

    Setter opp grant for å kunne slette tabell

--- src/main/resources/db/migration/V30__grant-privs.sql
+++ src/main/resources/db/migration/V30__grant-privs.sql
@@ -1,1 +1,15 @@
-grant all on all tables in schema public to cloudsqliamuser;
+create or replace procedure grant_all_cloudsqliamuser()
+    language plpgsql
+as
+$$
+declare
+begin
+    IF (SELECT exists(SELECT rolname FROM pg_roles WHERE rolname = 'cloudsqliamuser')) THEN
+        grant all on all tables in schema public to cloudsqliamuser;
+END IF;
+end;
+$$;
+
+call grant_all_cloudsqliamuser();
+
+drop procedure grant_all_cloudsqliamuser;
