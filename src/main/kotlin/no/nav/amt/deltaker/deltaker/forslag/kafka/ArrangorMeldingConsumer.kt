9ee82e95f8cb5b2df2e4657533a635c73db25f55
Izis Frøyhov <izisfroyhov@gmail.com>
2025-03-07 09:18:41 +0100

    Gjør klar lagring av vurderinger fra kafka som skal brukes når vi blir master for gruppeAMO (#354)

--- src/main/kotlin/no/nav/amt/deltaker/deltaker/forslag/kafka/ArrangorMeldingConsumer.kt
+++ src/main/kotlin/no/nav/amt/deltaker/deltaker/forslag/kafka/ArrangorMeldingConsumer.kt
@@ -5,6 +5,10 @@ import no.nav.amt.deltaker.Environment
 import no.nav.amt.deltaker.application.plugins.objectMapper
 import no.nav.amt.deltaker.deltaker.DeltakerService
 import no.nav.amt.deltaker.deltaker.forslag.ForslagService
+import no.nav.amt.deltaker.deltaker.kafka.DeltakerProducerService
+import no.nav.amt.deltaker.deltaker.kafka.toVurdering
+import no.nav.amt.deltaker.deltaker.vurdering.VurderingService
+import no.nav.amt.deltaker.unleash.UnleashToggle
 import no.nav.amt.lib.kafka.Consumer
 import no.nav.amt.lib.kafka.ManagedKafkaConsumer
 import no.nav.amt.lib.kafka.config.KafkaConfig
@@ -14,6 +18,7 @@ import no.nav.amt.lib.models.arrangor.melding.EndringFraArrangor
 import no.nav.amt.lib.models.arrangor.melding.Forslag
 import no.nav.amt.lib.models.arrangor.melding.Melding
 import no.nav.amt.lib.models.arrangor.melding.Vurdering
+import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakstype
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.kafka.common.serialization.UUIDDeserializer
 import org.slf4j.LoggerFactory
@@ -22,6 +27,9 @@ import java.util.UUID
 class ArrangorMeldingConsumer(
     private val forslagService: ForslagService,
     private val deltakerService: DeltakerService,
+    private val vurderingService: VurderingService,
+    private val deltakerProducerService: DeltakerProducerService,
+    private val unleashToggle: UnleashToggle,
     private val isDev: Boolean = Environment.isDev(),
     kafkaConfig: KafkaConfig = if (Environment.isLocal()) LocalKafkaConfig() else KafkaConfigImpl("earliest"),
 ) : Consumer<UUID, String?> {
@@ -57,8 +65,18 @@ class ArrangorMeldingConsumer(
         when (melding) {
             is EndringFraArrangor -> deltakerService.upsertEndretDeltaker(melding)
             is Forslag -> forslagService.upsert(melding)
-            is Vurdering -> {}
+            is Vurdering -> {
+                if (unleashToggle.erKometMasterForTiltakstype(Tiltakstype.ArenaKode.GRUPPEAMO)) {
+                    handleVurdering(melding)
+                }
+            }
         }
+    }
+
+    private suspend fun handleVurdering(vurdering: Vurdering) {
+        vurderingService.upsert(vurdering.toVurdering())
+        val deltaker = deltakerService.get(vurdering.deltakerId).getOrThrow()
+        deltakerProducerService.produce(deltaker)
     }
 
     override fun run() = consumer.run()
