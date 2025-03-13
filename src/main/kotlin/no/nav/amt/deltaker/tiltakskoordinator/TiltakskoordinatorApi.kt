f9ebe55ede908df632a9829b734ff6e8ef02d2fe
Tormod Fossum <tormod.fossum@nav.no>
2025-03-13 14:44:13 +0100

    Fjern midlertidig endepunkt

--- src/main/kotlin/no/nav/amt/deltaker/tiltakskoordinator/TiltakskoordinatorApi.kt
+++ src/main/kotlin/no/nav/amt/deltaker/tiltakskoordinator/TiltakskoordinatorApi.kt
@@ -5,10 +5,8 @@ import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Routing
 import io.ktor.server.routing.post
-import no.nav.amt.deltaker.auth.AuthorizationException
 import no.nav.amt.deltaker.deltaker.DeltakerService
 import no.nav.amt.deltaker.deltaker.model.Deltaker
-import no.nav.amt.deltaker.internal.isInternal
 import no.nav.amt.lib.models.tiltakskoordinator.requests.DelMedArrangorRequest
 import no.nav.amt.lib.models.tiltakskoordinator.response.EndringFraTiltakskoordinatorResponse
 
@@ -22,18 +20,6 @@ fun Routing.registerTiltakskoordinatorApi(deltakerService: DeltakerService) {
             val deltakere = deltakerService.upsertEndretDeltakere(request)
             call.respond(deltakere.map { it.toResponse() })
         }
-    }
-
-    // Endepunkt for å teste... Skal fjernes før merge
-    post("/internal$apiPath/del-med-arrangor") {
-        if (!isInternal(call.request.local.remoteAddress)) {
-            throw AuthorizationException("Ikke tilgang til api")
-        }
-
-        val request = call.receive<DelMedArrangorRequest>()
-
-        val deltakere = deltakerService.upsertEndretDeltakere(request)
-        call.respond(deltakere.map { it.toResponse() })
     }
 }
 
