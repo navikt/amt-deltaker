c164cd2542d1d7606dad0b5b539c29d74f49e501
Roar Skinderviken <roar.skinderviken@gmail.com>
2025-06-25 15:55:50 +0200

    Flyttet alle versjoner til libs.versions.toml. Oppdatert Gradle wrapper til 8.14.2.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,14 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-
 group = "no.nav.amt-deltaker"
 version = "1.0-SNAPSHOT"
 
-plugins {
-    val kotlinVersion = "2.1.21"
+val ktlintVersion = "1.2.1"
 
-    kotlin("jvm") version kotlinVersion
-    id("io.ktor.plugin") version "3.2.0"
-    id("org.jetbrains.kotlin.plugin.serialization") version kotlinVersion
-    id("org.jlleitschuh.gradle.ktlint") version "12.3.0"
-    id("com.gradleup.shadow") version "8.3.6"
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
+    alias(libs.plugins.kotlin.plugin.serialization)
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.shadow)
 }
 
 repositories {
@@ -18,76 +16,54 @@ repositories {
     maven { setUrl("https://github-package-registry-mirror.gc.nav.no/cached/maven-release") }
 }
 
-val kotlinVersion = "2.1.0"
-val ktorVersion = "3.2.0"
-val logbackVersion = "1.5.18"
-val prometeusVersion = "1.15.1"
-val ktlintVersion = "1.2.1"
-val jacksonVersion = "2.19.1"
-val logstashEncoderVersion = "8.1"
-val commonVersion = "3.2024.10.25_13.44-9db48a0dbe67"
-val poaoTilgangVersion = "2024.10.29_14.10-4fff597d6e1b"
-val kafkaClientsVersion = "3.7.1"
-val kotestVersion = "5.9.1"
-val flywayVersion = "11.9.1"
-val hikariVersion = "6.3.0"
-val kotliqueryVersion = "1.9.1"
-val postgresVersion = "42.7.7"
-val caffeineVersion = "3.2.1"
-val mockkVersion = "1.14.2"
-val nimbusVersion = "10.3"
-val unleashVersion = "11.0.0"
-val amtLibVersion = "1.2025.06.05_08.25-2338e0f39f58"
-val awaitilityVersion = "4.3.0"
-
 dependencies {
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
     implementation("io.ktor:ktor-serialization-jackson-jvm")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
+    implementation(libs.jackson.datatype.jsr310)
 
     implementation("io.ktor:ktor-server-call-logging-jvm")
     implementation("io.ktor:ktor-server-call-id-jvm")
     implementation("io.ktor:ktor-server-metrics-micrometer-jvm")
-    implementation("io.micrometer:micrometer-registry-prometheus:$prometeusVersion")
+    implementation(libs.micrometer.registry.prometheus)
     implementation("io.ktor:ktor-server-host-common-jvm")
     implementation("io.ktor:ktor-server-status-pages-jvm")
-    implementation("io.ktor:ktor-server-auth:$ktorVersion")
-    implementation("io.ktor:ktor-server-auth-jwt:$ktorVersion")
+    implementation(libs.ktor.server.auth)
+    implementation(libs.ktor.server.auth.jwt)
     implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("io.ktor:ktor-client-core:$ktorVersion")
-    implementation("io.ktor:ktor-client-apache:$ktorVersion")
-    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.apache)
+    implementation(libs.ktor.client.content.negotiation)
 
-    implementation("com.github.ben-manes.caffeine:caffeine:$caffeineVersion")
+    implementation(libs.caffeine)
 
-    implementation("ch.qos.logback:logback-classic:$logbackVersion")
-    implementation("net.logstash.logback:logstash-logback-encoder:$logstashEncoderVersion")
-    implementation("no.nav.common:log:$commonVersion")
+    implementation(libs.logback.classic)
+    implementation(libs.logstash.logback.encoder)
+    implementation(libs.nav.common.log)
 
-    implementation("no.nav.amt.lib:kafka:$amtLibVersion")
-    implementation("no.nav.amt.lib:utils:$amtLibVersion")
-    implementation("no.nav.amt.lib:models:$amtLibVersion")
+    implementation(libs.nav.amt.lib.kafka)
+    implementation(libs.nav.amt.lib.utils)
+    implementation(libs.nav.amt.lib.models)
 
-    implementation("no.nav.poao-tilgang:client:$poaoTilgangVersion")
+    implementation(libs.nav.poao.tilgang.client)
 
-    implementation("com.zaxxer:HikariCP:$hikariVersion")
-    implementation("org.flywaydb:flyway-core:$flywayVersion")
-    implementation("org.flywaydb:flyway-database-postgresql:$flywayVersion")
-    implementation("org.postgresql:postgresql:$postgresVersion")
-    implementation("com.github.seratch:kotliquery:$kotliqueryVersion")
-    implementation("io.getunleash:unleash-client-java:$unleashVersion")
+    implementation(libs.hikari)
+    implementation(libs.flyway.core)
+    implementation(libs.flyway.database.postgresql)
+    implementation(libs.postgresql)
+    implementation(libs.kotliquery)
+    implementation(libs.unleash.client.java)
 
     testImplementation("io.ktor:ktor-server-test-host")
-    testImplementation("io.ktor:ktor-client-mock:$ktorVersion")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
-    testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
-    testImplementation("io.kotest:kotest-assertions-json-jvm:$kotestVersion")
-    testImplementation("io.mockk:mockk:$mockkVersion")
-    testImplementation("com.nimbusds:nimbus-jose-jwt:$nimbusVersion")
-    testImplementation("no.nav.amt.lib:testing:$amtLibVersion")
-    testImplementation("org.awaitility:awaitility:$awaitilityVersion")
+    implementation(libs.ktor.client.mock)
+    implementation(libs.kotlin.test.junit)
+    implementation(libs.kotest.assertions.core.jvm)
+    implementation(libs.kotest.assertions.json.jvm)
+    implementation(libs.mockk)
+    implementation(libs.nimbus.jose.jwt)
+    implementation(libs.nav.amt.lib.testing)
+    implementation(libs.awaitility)
 }
 
 kotlin {
@@ -95,10 +71,14 @@ kotlin {
 }
 
 application {
-    mainClass.set("no.nav.amt.deltaker.ApplicationKt")
+    mainClass = "no.nav.amt.deltaker.ApplicationKt"
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+ktlint {
+    version = ktlintVersion
 }
 
 tasks.jar {
@@ -109,10 +89,6 @@ tasks.jar {
     }
 }
 
-tasks.withType<ShadowJar> {
+tasks.shadowJar {
     mergeServiceFiles()
-}
-
-configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
-    version.set(ktlintVersion)
 }
