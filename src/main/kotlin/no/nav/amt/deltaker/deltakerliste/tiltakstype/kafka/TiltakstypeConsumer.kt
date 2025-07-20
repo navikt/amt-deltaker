2fb3b59544e13b29260748453e78a11e57e42480
Roar Skinderviken <roar.skinderviken@gmail.com>
2025-07-20 16:10:29 +0200

    Refakturert bort duplikatkode i Kafka Consumer-klasser

--- src/main/kotlin/no/nav/amt/deltaker/deltakerliste/tiltakstype/kafka/TiltakstypeConsumer.kt
+++ src/main/kotlin/no/nav/amt/deltaker/deltakerliste/tiltakstype/kafka/TiltakstypeConsumer.kt
@@ -4,32 +4,18 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import no.nav.amt.deltaker.Environment
 import no.nav.amt.deltaker.application.plugins.objectMapper
 import no.nav.amt.deltaker.deltakerliste.tiltakstype.TiltakstypeRepository
+import no.nav.amt.deltaker.utils.buildManagedKafkaConsumer
 import no.nav.amt.lib.kafka.Consumer
-import no.nav.amt.lib.kafka.ManagedKafkaConsumer
-import no.nav.amt.lib.kafka.config.KafkaConfig
-import no.nav.amt.lib.kafka.config.KafkaConfigImpl
-import no.nav.amt.lib.kafka.config.LocalKafkaConfig
 import no.nav.amt.lib.models.deltakerliste.tiltakstype.kafka.TiltakstypeDto
-import org.apache.kafka.common.serialization.StringDeserializer
-import org.apache.kafka.common.serialization.UUIDDeserializer
-import org.slf4j.LoggerFactory
 import java.util.UUID
 
 class TiltakstypeConsumer(
     private val repository: TiltakstypeRepository,
-    kafkaConfig: KafkaConfig = if (Environment.isLocal()) LocalKafkaConfig() else KafkaConfigImpl("earliest"),
 ) : Consumer<UUID, String?> {
-    private val consumer = ManagedKafkaConsumer(
+    private val consumer = buildManagedKafkaConsumer(
         topic = Environment.TILTAKSTYPE_TOPIC,
-        config = kafkaConfig.consumerConfig(
-            keyDeserializer = UUIDDeserializer(),
-            valueDeserializer = StringDeserializer(),
-            groupId = Environment.KAFKA_CONSUMER_GROUP_ID,
-        ),
-        consume = ::consume,
+        consumeFunc = ::consume,
     )
-
-    private val log = LoggerFactory.getLogger(javaClass)
 
     override fun start() = consumer.start()
 
