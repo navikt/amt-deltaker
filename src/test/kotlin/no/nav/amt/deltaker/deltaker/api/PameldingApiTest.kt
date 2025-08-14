dba774c41176ac67898d6b31ac0aa570d49751a0
Roar Skinderviken <roar.skinderviken@nav.no>
2025-08-14 19:58:38 +0200

    Enablet tester

--- src/test/kotlin/no/nav/amt/deltaker/deltaker/api/PameldingApiTest.kt
+++ src/test/kotlin/no/nav/amt/deltaker/deltaker/api/PameldingApiTest.kt
@@ -6,6 +6,7 @@ import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.HttpStatusCode
+import io.ktor.server.plugins.requestvalidation.ValidationResult
 import io.ktor.server.testing.ApplicationTestBuilder
 import io.ktor.server.testing.testApplication
 import io.mockk.Runs
@@ -17,26 +18,30 @@ import no.nav.amt.deltaker.application.plugins.configureAuthentication
 import no.nav.amt.deltaker.application.plugins.configureRouting
 import no.nav.amt.deltaker.application.plugins.configureSerialization
 import no.nav.amt.deltaker.deltaker.DeltakerHistorikkService
+import no.nav.amt.deltaker.deltaker.OpprettKladdRequestValidator
 import no.nav.amt.deltaker.deltaker.PameldingService
 import no.nav.amt.deltaker.deltaker.api.model.request.AvbrytUtkastRequest
 import no.nav.amt.deltaker.deltaker.api.model.request.OpprettKladdRequest
 import no.nav.amt.deltaker.deltaker.api.model.request.UtkastRequest
 import no.nav.amt.deltaker.deltaker.api.model.toDeltakerEndringResponse
+import no.nav.amt.deltaker.deltaker.api.model.toKladdResponse
 import no.nav.amt.deltaker.deltaker.api.utils.noBodyRequest
 import no.nav.amt.deltaker.deltaker.api.utils.postRequest
 import no.nav.amt.deltaker.utils.configureEnvForAuthentication
 import no.nav.amt.deltaker.utils.data.TestData
+import no.nav.amt.deltaker.utils.data.TestData.lagDeltaker
+import no.nav.amt.deltaker.utils.data.TestData.lagDeltakerStatus
 import no.nav.amt.lib.models.deltaker.Deltakelsesinnhold
 import no.nav.amt.lib.models.deltaker.DeltakerHistorikk
 import no.nav.amt.lib.models.deltaker.DeltakerStatus
 import no.nav.amt.lib.models.deltaker.Innhold
 import no.nav.amt.lib.utils.objectMapper
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import java.util.UUID
 
 class PameldingApiTest {
+    private val opprettKladdRequestValidator = mockk<OpprettKladdRequestValidator>()
     private val pameldingService = mockk<PameldingService>()
     private val historikkService: DeltakerHistorikkService = mockk()
 
@@ -57,38 +62,47 @@ class PameldingApiTest {
     }
 
     @Test
-    @Disabled("TODO")
     fun `post pamelding - har tilgang - returnerer deltaker`() = testApplication {
-        val deltaker = TestData.lagDeltaker()
+        val deltaker = lagDeltaker()
 
-        coEvery { pameldingService.opprettDeltaker(any(), any()) } returns deltaker
+        coEvery {
+            opprettKladdRequestValidator.validateRequest(any())
+        } returns ValidationResult.Valid
+
+        coEvery {
+            pameldingService.opprettDeltaker(any(), any())
+        } returns deltaker
 
         setUpTestApplication()
 
-        client.post("/pamelding") { postRequest(opprettKladdRequest) }.apply {
-            status shouldBe HttpStatusCode.OK
-            bodyAsText() shouldBe objectMapper.writeValueAsString(deltaker)
-        }
+        val response = client.post("/pamelding") { postRequest(opprettKladdRequest) }
+
+        response.status shouldBe HttpStatusCode.OK
+        response.bodyAsText() shouldBe objectMapper.writeValueAsString(deltaker.toKladdResponse())
     }
 
     @Test
-    @Disabled("TODO")
     fun `post pamelding - deltakerliste finnes ikke - returnerer 404`() = testApplication {
         coEvery {
-            pameldingService.opprettDeltaker(
-                any(),
-                any(),
-            )
+            opprettKladdRequestValidator.validateRequest(any())
+        } returns ValidationResult.Valid
+
+        coEvery {
+            pameldingService.opprettDeltaker(any(), any())
         } throws NoSuchElementException("Fant ikke deltakerliste")
+
         setUpTestApplication()
-        client.post("/pamelding") { postRequest(opprettKladdRequest) }.apply {
-            status shouldBe HttpStatusCode.NotFound
+
+        val response = client.post("/pamelding") {
+            postRequest(opprettKladdRequest)
         }
+
+        response.status shouldBe HttpStatusCode.NotFound
     }
 
     @Test
     fun `post pamelding utkast - har tilgang - returnerer 200`() = testApplication {
-        val deltaker = TestData.lagDeltaker(status = TestData.lagDeltakerStatus(type = DeltakerStatus.Type.UTKAST_TIL_PAMELDING))
+        val deltaker = lagDeltaker(status = lagDeltakerStatus(type = DeltakerStatus.Type.UTKAST_TIL_PAMELDING))
         val historikk: List<DeltakerHistorikk> = listOf(DeltakerHistorikk.Vedtak(TestData.lagVedtak(deltakerVedVedtak = deltaker)))
 
         coEvery { historikkService.getForDeltaker(deltaker.id) } returns historikk
@@ -144,19 +158,19 @@ class PameldingApiTest {
             configureSerialization()
             configureAuthentication(Environment())
             configureRouting(
-                opprettKladdRequestValidator = mockk(),
-                pameldingService,
+                opprettKladdRequestValidator = opprettKladdRequestValidator,
+                pameldingService = pameldingService,
                 deltakerService = mockk(),
-                historikkService,
-                mockk(),
-                mockk(),
-                mockk(),
-                mockk(),
-                mockk(),
-                mockk(),
-                mockk(),
-                mockk(),
-                mockk(),
+                deltakerHistorikkService = historikkService,
+                tilgangskontrollService = mockk(),
+                deltakelserResponseMapper = mockk(),
+                deltakerProducerService = mockk(),
+                vedtakService = mockk(),
+                unleashToggle = mockk(),
+                innsokPaaFellesOppstartService = mockk(),
+                vurderingService = mockk(),
+                hendelseService = mockk(),
+                endringFraTiltakskoordinatorService = mockk(),
             )
         }
     }
