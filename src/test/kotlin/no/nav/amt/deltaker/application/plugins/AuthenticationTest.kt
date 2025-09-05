71331a4e4431e9ed97a12a9c76cdba2f8947bbea
Roar Skinderviken <7499642+roar-skinderviken@users.noreply.github.com>
2025-09-05 11:07:25 +0200

    Refakturert bort duplikatkode i tester (#552)

--- src/test/kotlin/no/nav/amt/deltaker/application/plugins/AuthenticationTest.kt
+++ src/test/kotlin/no/nav/amt/deltaker/application/plugins/AuthenticationTest.kt
@@ -1,137 +1,80 @@
 package no.nav.amt.deltaker.application.plugins
 
 import io.kotest.matchers.shouldBe
+import io.ktor.client.request.bearerAuth
 import io.ktor.client.request.get
-import io.ktor.client.request.header
 import io.ktor.client.statement.bodyAsText
-import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpStatusCode
-import io.ktor.server.application.Application
-import io.ktor.server.auth.authenticate
-import io.ktor.server.response.respondText
-import io.ktor.server.routing.get
-import io.ktor.server.routing.routing
-import io.ktor.server.testing.ApplicationTestBuilder
-import io.ktor.server.testing.testApplication
-import io.mockk.mockk
-import no.nav.amt.deltaker.Environment
-import no.nav.amt.deltaker.utils.configureEnvForAuthentication
+import no.nav.amt.deltaker.utils.RouteTestBase
 import no.nav.amt.deltaker.utils.generateJWT
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
-class AuthenticationTest {
-    @BeforeEach
-    fun setup() {
-        configureEnvForAuthentication()
-    }
-
+class AuthenticationTest : RouteTestBase() {
     @Test
-    fun `testAuthentication - gyldig token, klient-app har tilgang - returnerer 200`() = testApplication {
-        setUpTestApplication()
-        client
-            .get("/deltaker") {
-                header(
-                    HttpHeaders.Authorization,
-                    "Bearer ${generateJWT(consumerClientId = "amt-deltaker-bff", audience = "amt-deltaker")}",
-                )
-            }.apply {
-                status shouldBe HttpStatusCode.OK
-                bodyAsText() shouldBe "System har tilgang!"
+    fun `testAuthentication - gyldig token, klient-app har tilgang - returnerer 200`() {
+        withTestApplicationContext { client ->
+            val response = client.get("/deltaker") {
+                bearerAuth(bearerTokenInTest)
             }
+
+            response.status shouldBe HttpStatusCode.OK
+            response.bodyAsText() shouldBe "System har tilgang!"
+        }
     }
 
     @Test
-    fun `testAuthentication - gyldig token, ikke maskin-til-maskin - returnerer 401`() = testApplication {
-        setUpTestApplication()
-        client
-            .get("/deltaker") {
-                header(
-                    HttpHeaders.Authorization,
-                    "Bearer ${generateJWT(consumerClientId = "amt-deltaker-bff", audience = "amt-deltaker", oid = "ikke-subject")}",
-                )
-            }.apply {
-                status shouldBe HttpStatusCode.Unauthorized
+    fun `testAuthentication - gyldig token, ikke maskin-til-maskin - returnerer 401`() {
+        val response = withTestApplicationContext { client ->
+            client.get("/deltaker") {
+                bearerAuth(generateJWT(consumerClientId = "amt-deltaker-bff", audience = "amt-deltaker", oid = "ikke-subject"))
             }
+        }
+        response.status shouldBe HttpStatusCode.Unauthorized
     }
 
     @Test
-    fun `testAuthentication - gyldig token, klient-app har ikke tilgang - returnerer 401`() = testApplication {
-        setUpTestApplication()
-        client
-            .get("/deltaker") {
-                header(
-                    HttpHeaders.Authorization,
-                    "Bearer ${generateJWT(consumerClientId = "annen-consumer", audience = "amt-deltaker")}",
-                )
-            }.apply {
-                status shouldBe HttpStatusCode.Unauthorized
+    fun `testAuthentication - gyldig token, klient-app har ikke tilgang - returnerer 401`() {
+        val response = withTestApplicationContext { client ->
+            client.get("/deltaker") {
+                bearerAuth(generateJWT(consumerClientId = "annen-consumer", audience = "amt-deltaker"))
             }
+        }
+        response.status shouldBe HttpStatusCode.Unauthorized
     }
 
     @Test
-    fun `testAuthentication - gyldig token, feil audience - returnerer 401`() = testApplication {
-        setUpTestApplication()
-        client
-            .get("/deltaker") {
-                header(
-                    HttpHeaders.Authorization,
-                    "Bearer ${generateJWT(consumerClientId = "amt-deltaker-bff", audience = "feil-aud")}",
-                )
-            }.apply {
-                status shouldBe HttpStatusCode.Unauthorized
+    fun `testAuthentication - gyldig token, feil audience - returnerer 401`() {
+        val response = withTestApplicationContext { client ->
+            client.get("/deltaker") {
+                bearerAuth(generateJWT(consumerClientId = "amt-deltaker-bff", audience = "feil-aud"))
             }
+        }
+
+        response.status shouldBe HttpStatusCode.Unauthorized
     }
 
     @Test
-    fun `testAuthentication - ugyldig tokenissuer - returnerer 401`() = testApplication {
-        setUpTestApplication()
-        client
-            .get("/deltaker") {
-                header(
-                    HttpHeaders.Authorization,
-                    "Bearer ${
+    fun `testAuthentication - ugyldig tokenissuer - returnerer 401`() {
+        val response = withTestApplicationContext { client ->
+            client
+                .get("/deltaker") {
+                    bearerAuth(
                         generateJWT(
                             consumerClientId = "amt-deltaker-bff",
                             audience = "amt-deltaker",
                             issuer = "annenIssuer",
-                        )
-                    }",
-                )
-            }.apply {
-                status shouldBe HttpStatusCode.Unauthorized
-            }
-    }
-
-    private fun ApplicationTestBuilder.setUpTestApplication() {
-        application {
-            configureSerialization()
-            configureAuthentication(Environment())
-            configureRouting(
-                pameldingService = mockk(),
-                deltakerService = mockk(),
-                deltakerHistorikkService = mockk(),
-                tilgangskontrollService = mockk(),
-                deltakelserResponseMapper = mockk(),
-                deltakerProducerService = mockk(),
-                vedtakService = mockk(),
-                unleashToggle = mockk(),
-                innsokPaaFellesOppstartService = mockk(),
-                vurderingService = mockk(),
-                hendelseService = mockk(),
-                endringFraTiltakskoordinatorService = mockk(),
-            )
-            setUpTestRoute()
-        }
-    }
-
-    private fun Application.setUpTestRoute() {
-        routing {
-            authenticate("SYSTEM") {
-                get("/deltaker") {
-                    call.respondText("System har tilgang!")
+                        ),
+                    )
                 }
-            }
         }
+
+        response.status shouldBe HttpStatusCode.Unauthorized
+    }
+
+    companion object {
+        private val bearerTokenInTest = generateJWT(
+            consumerClientId = "amt-deltaker-bff",
+            audience = "amt-deltaker",
+        )
     }
 }
--- src/test/kotlin/no/nav/amt/deltaker/deltaker/api/PameldingApiTest.kt
+++ src/test/kotlin/no/nav/amt/deltaker/deltaker/api/PameldingApiTest.kt
@@ -8,25 +8,14 @@ import io.ktor.client.request.setBody
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.plugins.requestvalidation.ValidationResult
-import io.ktor.server.testing.ApplicationTestBuilder
-import io.ktor.server.testing.testApplication
 import io.mockk.Runs
 import io.mockk.coEvery
 import io.mockk.just
-import io.mockk.mockk
-import no.nav.amt.deltaker.Environment
-import no.nav.amt.deltaker.application.plugins.configureAuthentication
-import no.nav.amt.deltaker.application.plugins.configureRequestValidation
-import no.nav.amt.deltaker.application.plugins.configureRouting
-import no.nav.amt.deltaker.application.plugins.configureSerialization
-import no.nav.amt.deltaker.deltaker.DeltakerHistorikkService
-import no.nav.amt.deltaker.deltaker.OpprettKladdRequestValidator
-import no.nav.amt.deltaker.deltaker.PameldingService
 import no.nav.amt.deltaker.deltaker.api.DtoMappers.opprettKladdResponseFromDeltaker
 import no.nav.amt.deltaker.deltaker.api.DtoMappers.utkastResponseFromDeltaker
 import no.nav.amt.deltaker.deltaker.api.utils.noBodyRequest
 import no.nav.amt.deltaker.deltaker.api.utils.postRequest
-import no.nav.amt.deltaker.utils.configureEnvForAuthentication
+import no.nav.amt.deltaker.utils.RouteTestBase
 import no.nav.amt.deltaker.utils.data.TestData
 import no.nav.amt.deltaker.utils.data.TestData.lagDeltaker
 import no.nav.amt.deltaker.utils.data.TestData.lagDeltakerStatus
@@ -38,47 +27,40 @@ import no.nav.amt.lib.models.deltaker.internalapis.paamelding.request.AvbrytUtka
 import no.nav.amt.lib.models.deltaker.internalapis.paamelding.request.OpprettKladdRequest
 import no.nav.amt.lib.models.deltaker.internalapis.paamelding.request.UtkastRequest
 import no.nav.amt.lib.utils.objectMapper
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.util.UUID
 
-class PameldingApiTest {
-    private val opprettKladdRequestValidator = mockk<OpprettKladdRequestValidator>()
-    private val pameldingService = mockk<PameldingService>()
-    private val historikkService: DeltakerHistorikkService = mockk()
-
-    @BeforeEach
-    fun setup() {
-        configureEnvForAuthentication()
+class PameldingApiTest : RouteTestBase() {
+    @Test
+    fun `skal teste autentisering - mangler token - returnerer 401`() {
+        withTestApplicationContext { client ->
+            client.post("/pamelding") { setBody("foo") }.status shouldBe HttpStatusCode.Unauthorized
+            client.post("/pamelding/${UUID.randomUUID()}") { setBody("foo") }.status shouldBe HttpStatusCode.Unauthorized
+            client.post("/pamelding/${UUID.randomUUID()}/innbygger/godkjenn-utkast") { setBody("foo") }.status shouldBe
+                HttpStatusCode.Unauthorized
+            client.post("/pamelding/${UUID.randomUUID()}/avbryt") { setBody("foo") }.status shouldBe HttpStatusCode.Unauthorized
+            client.delete("/pamelding/${UUID.randomUUID()}").status shouldBe HttpStatusCode.Unauthorized
+        }
     }
 
     @Test
-    fun `skal teste autentisering - mangler token - returnerer 401`() = testApplication {
-        setUpTestApplication()
-        client.post("/pamelding") { setBody("foo") }.status shouldBe HttpStatusCode.Unauthorized
-        client.post("/pamelding/${UUID.randomUUID()}") { setBody("foo") }.status shouldBe HttpStatusCode.Unauthorized
-        client.post("/pamelding/${UUID.randomUUID()}/innbygger/godkjenn-utkast") { setBody("foo") }.status shouldBe
-            HttpStatusCode.Unauthorized
-        client.post("/pamelding/${UUID.randomUUID()}/avbryt") { setBody("foo") }.status shouldBe HttpStatusCode.Unauthorized
-        client.delete("/pamelding/${UUID.randomUUID()}").status shouldBe HttpStatusCode.Unauthorized
-    }
-
-    @Test
-    fun `post pamelding - request med valideringsfeil - returnerer 400 BadRequest`() = testApplication {
+    fun `post pamelding - request med valideringsfeil - returnerer 400 BadRequest`() {
         coEvery {
             opprettKladdRequestValidator.validateRequest(any())
         } returns ValidationResult.Invalid(listOf("~some error~", "~some other error~"))
 
-        setUpTestApplication()
+        withTestApplicationContext<Unit> { client ->
+            val response = client.post("/pamelding") {
+                postRequest(opprettKladdRequest)
+            }
 
-        val response = client.post("/pamelding") { postRequest(opprettKladdRequest) }
-
-        response.status shouldBe HttpStatusCode.BadRequest
-        response.bodyAsText() shouldContain ("~some error~, ~some other error~")
+            response.status shouldBe HttpStatusCode.BadRequest
+            response.bodyAsText() shouldContain ("~some error~, ~some other error~")
+        }
     }
 
     @Test
-    fun `post pamelding - har tilgang - returnerer deltaker`() = testApplication {
+    fun `post pamelding - har tilgang - returnerer deltaker`() {
         val deltaker = lagDeltaker()
 
         coEvery {
@@ -89,16 +71,18 @@ class PameldingApiTest {
             pameldingService.opprettDeltaker(any(), any())
         } returns deltaker
 
-        setUpTestApplication()
+        withTestApplicationContext { client ->
+            val response = client.post("/pamelding") {
+                postRequest(opprettKladdRequest)
+            }
 
-        val response = client.post("/pamelding") { postRequest(opprettKladdRequest) }
-
-        response.status shouldBe HttpStatusCode.OK
-        response.bodyAsText() shouldBe objectMapper.writeValueAsString(opprettKladdResponseFromDeltaker(deltaker))
+            response.status shouldBe HttpStatusCode.OK
+            response.bodyAsText() shouldBe objectMapper.writeValueAsString(opprettKladdResponseFromDeltaker(deltaker))
+        }
     }
 
     @Test
-    fun `post pamelding - deltakerliste finnes ikke - returnerer 404`() = testApplication {
+    fun `post pamelding - deltakerliste finnes ikke - returnerer 404`() {
         coEvery {
             opprettKladdRequestValidator.validateRequest(any())
         } returns ValidationResult.Valid
@@ -107,100 +91,88 @@ class PameldingApiTest {
             pameldingService.opprettDeltaker(any(), any())
         } throws NoSuchElementException("Fant ikke deltakerliste")
 
-        setUpTestApplication()
+        withTestApplicationContext { client ->
+            val response = client.post("/pamelding") {
+                postRequest(opprettKladdRequest)
+            }
 
-        val response = client.post("/pamelding") {
-            postRequest(opprettKladdRequest)
+            response.status shouldBe HttpStatusCode.NotFound
         }
-
-        response.status shouldBe HttpStatusCode.NotFound
     }
 
     @Test
-    fun `post pamelding utkast - har tilgang - returnerer 200`() = testApplication {
+    fun `post pamelding utkast - har tilgang - returnerer 200`() {
         val deltaker = lagDeltaker(status = lagDeltakerStatus(type = DeltakerStatus.Type.UTKAST_TIL_PAMELDING))
         val historikk: List<DeltakerHistorikk> = listOf(DeltakerHistorikk.Vedtak(TestData.lagVedtak(deltakerVedVedtak = deltaker)))
 
-        coEvery { historikkService.getForDeltaker(deltaker.id) } returns historikk
-
+        coEvery { deltakerHistorikkService.getForDeltaker(deltaker.id) } returns historikk
         coEvery { pameldingService.upsertUtkast(deltaker.id, any()) } returns deltaker
 
-        setUpTestApplication()
-
-        client.post("/pamelding/${deltaker.id}") { postRequest(utkastRequest) }.apply {
-            status shouldBe HttpStatusCode.OK
-            bodyAsText() shouldBe objectMapper.writeValueAsString(utkastResponseFromDeltaker(deltaker, historikk))
+        withTestApplicationContext { client ->
+            client
+                .post("/pamelding/${deltaker.id}") {
+                    postRequest(utkastRequest)
+                }.apply {
+                    status shouldBe HttpStatusCode.OK
+                    bodyAsText() shouldBe objectMapper.writeValueAsString(utkastResponseFromDeltaker(deltaker, historikk))
+                }
         }
     }
 
     @Test
-    fun `post pamelding utkast - deltaker finnes ikke - returnerer 404`() = testApplication {
+    fun `post pamelding utkast - deltaker finnes ikke - returnerer 404`() {
         val deltakerId = UUID.randomUUID()
         coEvery { pameldingService.upsertUtkast(deltakerId, any()) } throws NoSuchElementException("Fant ikke deltaker")
 
-        setUpTestApplication()
-
-        client.post("/pamelding/$deltakerId") { postRequest(utkastRequest) }.apply {
-            status shouldBe HttpStatusCode.NotFound
+        withTestApplicationContext { client ->
+            client
+                .post("/pamelding/$deltakerId") {
+                    postRequest(utkastRequest)
+                }.apply {
+                    status shouldBe HttpStatusCode.NotFound
+                }
         }
     }
 
     @Test
-    fun `post avbryt utkast - har tilgang - returnerer 200`() = testApplication {
+    fun `post avbryt utkast - har tilgang - returnerer 200`() {
         val deltakerId = UUID.randomUUID()
         coEvery { pameldingService.avbrytUtkast(deltakerId, any()) } just Runs
 
-        setUpTestApplication()
-
-        client.post("/pamelding/$deltakerId/avbryt") { postRequest(avbrytUtkastRequest) }.apply {
-            status shouldBe HttpStatusCode.OK
+        withTestApplicationContext { client ->
+            client
+                .post("/pamelding/$deltakerId/avbryt") {
+                    postRequest(avbrytUtkastRequest)
+                }.apply {
+                    status shouldBe HttpStatusCode.OK
+                }
         }
     }
 
     @Test
-    fun `delete kladd - har tilgang - returnerer 200`() = testApplication {
+    fun `delete kladd - har tilgang - returnerer 200`() {
         val deltakerId = UUID.randomUUID()
         coEvery { pameldingService.slettKladd(deltakerId) } just Runs
 
-        setUpTestApplication()
-
-        client.delete("/pamelding/$deltakerId") { noBodyRequest() }.apply {
-            status shouldBe HttpStatusCode.OK
+        withTestApplicationContext { client ->
+            client.delete("/pamelding/$deltakerId") { noBodyRequest() }.apply {
+                status shouldBe HttpStatusCode.OK
+            }
         }
     }
 
-    private fun ApplicationTestBuilder.setUpTestApplication() {
-        application {
-            configureSerialization()
-            configureAuthentication(Environment())
-            configureRequestValidation(opprettKladdRequestValidator = opprettKladdRequestValidator)
-            configureRouting(
-                pameldingService = pameldingService,
-                deltakerService = mockk(),
-                deltakerHistorikkService = historikkService,
-                tilgangskontrollService = mockk(),
-                deltakelserResponseMapper = mockk(),
-                deltakerProducerService = mockk(),
-                vedtakService = mockk(),
-                unleashToggle = mockk(),
-                innsokPaaFellesOppstartService = mockk(),
-                vurderingService = mockk(),
-                hendelseService = mockk(),
-                endringFraTiltakskoordinatorService = mockk(),
-            )
-        }
+    companion object {
+        private val opprettKladdRequest = OpprettKladdRequest(UUID.randomUUID(), "1234")
+        private val avbrytUtkastRequest = AvbrytUtkastRequest("Z123456", "0101")
+
+        private val utkastRequest = UtkastRequest(
+            Deltakelsesinnhold("test", listOf(Innhold("Tekst", "kode", true, null))),
+            "bakgrunn og sånn",
+            50F,
+            3F,
+            "Z123456",
+            "0101",
+            false,
+        )
     }
-
-    private val opprettKladdRequest = OpprettKladdRequest(UUID.randomUUID(), "1234")
-
-    private val utkastRequest = UtkastRequest(
-        Deltakelsesinnhold("test", listOf(Innhold("Tekst", "kode", true, null))),
-        "bakgrunn og sånn",
-        50F,
-        3F,
-        "Z123456",
-        "0101",
-        false,
-    )
-    private val avbrytUtkastRequest = AvbrytUtkastRequest("Z123456", "0101")
 }
