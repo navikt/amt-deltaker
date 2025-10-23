6e13baafc687f54bdb34a3724b2dcada7d727401
Roar Skinderviken <roar.skinderviken@gmail.com>
2025-10-23 16:19:37 +0200

    Fikset Flyway migrering #2

--- src/main/resources/db/migration/V39__datastream-grants.sql
+++ src/main/resources/db/migration/V39__datastream-grants.sql
@@ -12,11 +12,13 @@ BEGIN
 END
 $$ LANGUAGE 'plpgsql';
 
+DO
+$$
 BEGIN
     IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'datastream') THEN
         IF NOT EXISTS (SELECT 1 FROM pg_replication_slots WHERE slot_name = 'ds_replication') THEN
-            PERFORM PG_CREATE_LOGICAL_REPLICATION_SLOT('ds_replication', 'pgoutput');
+            PERFORM pg_create_logical_replication_slot('ds_replication', 'pgoutput');
         END IF;
     END IF;
 END
-$$ LANGUAGE 'plpgsql';
+$$ LANGUAGE plpgsql;
