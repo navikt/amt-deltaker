78ee00997888d4fbd5939c5d88508c0aed727e3f
Roar Skinderviken <roar.skinderviken@gmail.com>
2025-10-23 15:31:59 +0200

    Lagt til migrering for datastream

---
+++ src/main/resources/db/migration/V39__datastream-grants.sql
@@ -0,0 +1,22 @@
+DO
+$$
+BEGIN
+    IF EXISTS(SELECT * FROM pg_roles WHERE rolname = 'datastream') THEN
+       ALTER USER "amt-deltaker-2" WITH REPLICATION;
+       CREATE PUBLICATION "ds_publication" FOR ALL TABLES;
+
+       ALTER DEFAULT PRIVILEGES IN SCHEMA PUBLIC GRANT SELECT ON TABLES TO "datastream";
+       GRANT SELECT ON ALL TABLES IN SCHEMA PUBLIC TO "datastream";
+       ALTER USER "datastream" WITH REPLICATION;
+    END IF;
+END
+$$ LANGUAGE 'plpgsql';
+
+DO
+$$
+BEGIN
+    IF EXISTS(SELECT * FROM pg_roles WHERE rolname = 'datastream') THEN
+       PERFORM PG_CREATE_LOGICAL_REPLICATION_SLOT('ds_replication', 'pgoutput');
+    END IF;
+END
+$$ LANGUAGE 'plpgsql';
