9cb1216ae1344ead7e0bce468d59db761fb82806
Roar Skinderviken <roar.skinderviken@gmail.com>
2025-11-11 12:17:43 +0100

    Bruker Any i stedet for String i stub

--- src/test/kotlin/no/nav/amt/deltaker/utils/RouteTestBase.kt
+++ src/test/kotlin/no/nav/amt/deltaker/utils/RouteTestBase.kt
@@ -73,8 +73,8 @@ abstract class RouteTestBase {
 
         mockkObject(Database)
 
-        coEvery { Database.transaction(any<suspend () -> String>()) } coAnswers {
-            val block = firstArg<suspend () -> String>()
+        coEvery { Database.transaction(any<suspend () -> Any>()) } coAnswers {
+            val block = firstArg<suspend () -> Any>()
             block()
         }
     }
