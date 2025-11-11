e5bb8884a47431a8750aee786e747f5bf34dcb46
Roar Skinderviken <7499642+roar-skinderviken@users.noreply.github.com>
2025-11-11 16:20:53 +0100

    Revert "Legger til transaksjoner på alle api endepunkter som gjør end… (#615)
    * Revert "Legger til transaksjoner på alle api endepunkter som gjør endringer (#614)"
    
    This reverts commit ca1a5e5f3cb4b1e384e85e95823fb58f6a0facc0.
    
    * Build fix

--- src/main/kotlin/no/nav/amt/deltaker/deltaker/api/paamelding/PameldingApi.kt
+++ src/main/kotlin/no/nav/amt/deltaker/deltaker/api/paamelding/PameldingApi.kt
@@ -14,59 +14,51 @@ import no.nav.amt.deltaker.deltaker.api.DtoMappers.utkastResponseFromDeltaker
 import no.nav.amt.lib.models.deltaker.internalapis.paamelding.request.AvbrytUtkastRequest
 import no.nav.amt.lib.models.deltaker.internalapis.paamelding.request.OpprettKladdRequest
 import no.nav.amt.lib.models.deltaker.internalapis.paamelding.request.UtkastRequest
-import no.nav.amt.lib.utils.database.Database
 import java.util.UUID
 
 fun Routing.registerPameldingApi(pameldingService: PameldingService, historikkService: DeltakerHistorikkService) {
     authenticate("SYSTEM") {
         post("/pamelding") {
             val opprettKladdRequest = call.receive<OpprettKladdRequest>()
-            Database.transaction {
-                val deltaker = pameldingService.opprettDeltaker(
-                    deltakerListeId = opprettKladdRequest.deltakerlisteId,
-                    personIdent = opprettKladdRequest.personident,
-                )
 
-                call.respond(opprettKladdResponseFromDeltaker(deltaker))
-            }
+            val deltaker = pameldingService.opprettDeltaker(
+                deltakerListeId = opprettKladdRequest.deltakerlisteId,
+                personIdent = opprettKladdRequest.personident,
+            )
+
+            call.respond(opprettKladdResponseFromDeltaker(deltaker))
         }
 
         post("/pamelding/{deltakerId}") {
             val request = call.receive<UtkastRequest>()
             val deltakerId = UUID.fromString(call.parameters["deltakerId"])
-            Database.transaction {
-                val deltaker = pameldingService.upsertUtkast(deltakerId, request)
-                val historikk = historikkService.getForDeltaker(deltaker.id)
-                call.respond(utkastResponseFromDeltaker(deltaker, historikk))
-            }
+
+            val deltaker = pameldingService.upsertUtkast(deltakerId, request)
+            val historikk = historikkService.getForDeltaker(deltaker.id)
+            call.respond(utkastResponseFromDeltaker(deltaker, historikk))
         }
 
         post("/pamelding/{deltakerId}/innbygger/godkjenn-utkast") {
             val deltakerId = UUID.fromString(call.parameters["deltakerId"])
-            Database.transaction {
-                val oppdatertDeltaker = pameldingService.innbyggerGodkjennUtkast(deltakerId)
-                val historikk = historikkService.getForDeltaker(oppdatertDeltaker.id)
 
-                call.respond(utkastResponseFromDeltaker(oppdatertDeltaker, historikk))
-            }
+            val oppdatertDeltaker = pameldingService.innbyggerGodkjennUtkast(deltakerId)
+            val historikk = historikkService.getForDeltaker(oppdatertDeltaker.id)
+
+            call.respond(utkastResponseFromDeltaker(oppdatertDeltaker, historikk))
         }
 
         post("/pamelding/{deltakerId}/avbryt") {
             val request = call.receive<AvbrytUtkastRequest>()
             val deltakerId = UUID.fromString(call.parameters["deltakerId"])
-            Database.transaction {
-                // throw UnsupportedOperationException("Tralala")
-                pameldingService.avbrytUtkast(deltakerId, request)
-                call.respond(HttpStatusCode.OK)
-            }
+
+            pameldingService.avbrytUtkast(deltakerId, request)
+            call.respond(HttpStatusCode.OK)
         }
 
         delete("/pamelding/{deltakerId}") {
             val deltakerId = UUID.fromString(call.parameters["deltakerId"])
-            Database.transaction {
-                pameldingService.slettKladd(deltakerId)
-                call.respond(HttpStatusCode.OK)
-            }
+            pameldingService.slettKladd(deltakerId)
+            call.respond(HttpStatusCode.OK)
         }
     }
 }
