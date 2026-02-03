eb148027b0822e608b4f94d2acf2a83153312077
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-02-03 22:51:43 +0100

    Lagt til fix for indeks vedtak_deltaker_id_unique i og med at constraint finnes i dev allerede. Dropper duplikatindeks vedtak_deltaker_id_idx

--- src/main/resources/db/migration/V51__create_vedtak_deltaker_id_unique.sql
+++ src/main/resources/db/migration/V51__create_vedtak_deltaker_id_unique.sql
@@ -1,1 +1,14 @@
-ALTER TABLE vedtak ADD CONSTRAINT vedtak_deltaker_id_unique UNIQUE (deltaker_id);
+DO $$
+    BEGIN
+        IF NOT EXISTS (
+            SELECT 1
+            FROM information_schema.table_constraints
+            WHERE
+                table_name='vedtak'
+                AND constraint_name='vedtak_deltaker_id_unique'
+        ) THEN
+            ALTER TABLE vedtak ADD CONSTRAINT vedtak_deltaker_id_unique UNIQUE (deltaker_id);
+        END IF;
+    END$$;
+
+DROP INDEX IF EXISTS vedtak_deltaker_id_idx;
