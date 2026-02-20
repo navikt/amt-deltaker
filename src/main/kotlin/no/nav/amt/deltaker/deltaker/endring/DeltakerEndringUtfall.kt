5e94353542f6605ecafee4dcfd39a23aab36eccb
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-02-20 22:38:18 +0100

    Liten refakturering av DeltakerEndringUtfall

--- src/main/kotlin/no/nav/amt/deltaker/deltaker/endring/DeltakerEndringUtfall.kt
+++ src/main/kotlin/no/nav/amt/deltaker/deltaker/endring/DeltakerEndringUtfall.kt
@@ -7,23 +7,27 @@ sealed interface DeltakerEndringUtfall {
     class VellykketEndring(
         val deltaker: Deltaker,
         val nesteStatus: DeltakerStatus? = null,
-    ) : DeltakerEndringUtfall
+    ) : DeltakerEndringUtfall {
+        val deltakerId: String get() = deltaker.id.toString()
+
+        override fun getOrThrow() = deltaker
+    }
 
     class FremtidigEndring(
         val deltaker: Deltaker,
-    ) : DeltakerEndringUtfall
+    ) : DeltakerEndringUtfall {
+        override fun getOrThrow() = deltaker
+    }
 
     class UgyldigEndring(
         val error: Throwable,
-    ) : DeltakerEndringUtfall
+    ) : DeltakerEndringUtfall {
+        override fun getOrThrow() = throw error
+    }
 
     val erVellykket: Boolean get() = this is VellykketEndring
 
     val erUgyldig: Boolean get() = this is UgyldigEndring
 
-    fun getOrThrow(): Deltaker = when (this) {
-        is VellykketEndring -> deltaker
-        is FremtidigEndring -> deltaker
-        is UgyldigEndring -> throw error
-    }
+    fun getOrThrow(): Deltaker
 }
